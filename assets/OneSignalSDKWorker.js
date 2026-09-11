d8277be22809cc5951224951df3af093b5348d59
Joseph <hill3742@gmail.com>
2026-09-10 22:23:11 -0500

    Bust service worker cache and expose worker registration status

--- assets/OneSignalSDKWorker.js
+++ assets/OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+try {
+  importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+} catch (e) {
+  console.error("Failed to load OneSignal worker:", e);
+}
