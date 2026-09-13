0b664dbe56dc34f5231cb030f0c44c11ff994f44
Joseph <hill3742@gmail.com>
2026-09-12 22:26:36 -0500

    Host OneSignal service worker script locally on same origin

--- assets/OneSignalSDKWorker.js
+++ assets/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-try {
-  importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-} catch (e) {
-  console.error("Failed to load OneSignal worker:", e);
-}
+importScripts("/OneSignalSDK.sw.js");
